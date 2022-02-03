b3e6bebab21f07f7726eadb89b90056974fdf709
Mikolaj <huntergerald00@gmail.com>
2022-02-03 20:47:01 +0100

    dodano logowanie i rejestracje do aplikacji mobilnej

---
+++ mobileApp/AuthScreenActivate.js
@@ -0,0 +1,38 @@
+import { useLinkProps } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import {stylesAuth} from './styles'
+import {AuthContext} from './App'
+import { HeaderBackButton } from '@react-navigation/stack';
+
+const AuthScreenActivate = ({navigation}) => {
+    const {activate} = React.useContext(AuthContext);
+
+    const [email, setEmail] = React.useState();
+    const [code, setCode] = React.useState();
+
+    const validate = () => {
+        //regex do sprawdzania maila
+        //sprawdzanie poprawdnosci kodu
+        
+        if(activate({email, code})){
+            navigation.navigate('Logowanie');
+        }
+    }
+
+    return(
+        <View style={stylesAuth.back}>
+            <View style={stylesAuth.card}>
+                <TextInput style={stylesAuth.input} placeholder='Adres email' onChangeText={setEmail}></TextInput>
+                <TextInput style={stylesAuth.input} placeholder='Kod aktywacyjny' onChangeText={setCode}></TextInput>
+                <Button
+                    title='Aktywuj konto'
+                    onPress={validate}
+                    style={{margin: 5}}
+                />
+            </View>
+        </View>
+    )
+}
+
+export default AuthScreenActivate;
---
+++ mobileApp/readme.txt
@@ -0,0 +1,3 @@
+Żeby uruchomić aplikację trzeba włączyć emulator albo podłączyć telefon po kablu (jeśli używamy fizycznego telefonu, trzeba zezwolić na instalowanie aplikacji z nieznanych źródeł, i w opcjach programistycznych włączyć "Debugowanie przez USB").
+Potem w folderze z aplikacją użyć polecenia: 'npx react-native start'.
+W App.js zmienna adresSerwera ustawic na adres komputera na ktorym dziala serwer.
